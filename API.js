69320160c3792fedf2b42950c6db3ffa5568bbcd
iVladyuser <korol.v09@gmail.com>
2023-09-08 17:53:48 +0200

    update

--- API.js
+++ API.js
@@ -1,6 +1,105 @@
+// const BASE_URL = "https://the-one-api.dev/v2";
+// const END_POINT = "/character";
+// const KEY = "22ykK7YCQ2YlFzE2keCs";
+
+// function getCharacter() {
+// 	const param = new URLSearchParams({
+// 		limit: 30,
+// 		page: 1,
+// 	});
+
+// 	const option = {
+//     //запит на авторизацыю
+//     method: "GET",
+//     // на headers робимо авторизацію.. оскільки тільки ми фізично на компі маємо доступ до headers
+// 		headers: {
+//       Authorization: `Bearer ${KEY}`,
+//       // якщо потрыбно щось створити method: POST, body:...
+// 		},
+// 	};
+
+// 	return fetch(`${BASE_URL}${END_POINT}?${param}`, option).then((response) =>
+// 		console.log(response)
+// 	);
+// }
+// getCharacter();
+
+// let options = {
+// 	// ми слідкуємо за всім екраном, а не його частиною
+// 	root: null,
+// 	rootMargin: "200px",
+// 	threshold: 1.0,
+// };
+
+// let observer = new IntersectionObserver(callback, options);
+// function callback(evt) {
+// 	console.log(evt);
+// }
+
+// const BASE_URL = "https://api.themoviedb.org/3/";
+// const END_POINT = "trending/movie/day";
+// const list = document.querySelector(".js-list");
+// const API_KEY = "345007f9ab440e5b86cef51be6397df1";
+// const loadMore = document.querySelector(".js-load");
+// let currentPage = 1;
+
+// // створюємо div за яким ми будемо слідкувати
+// const target = document.querySelector(".js-guard");
+
+// loadMore.addEventListener("click", onLoad);
+
+// function onLoad() {
+// 	currentPage += 1;
+// 	getTrending(currentPage)
+// 		.then((data) => {
+// 			list.insertAdjacentHTML("beforeend", createMarkup(data.results));
+// 			//якщо ми на останній сторінці кнопки нема
+// 			if (data.page === data.total_pages) {
+// 				loadMore.hidden = true;
+// 			}
+// 		})
+// 		.catch((err) => console.log(err));
+// }
+
+// function getTrending(page = 1) {
+// 	return fetch(`${BASE_URL}${END_POINT}?api_key=${API_KEY}&page=${page}`).then(
+// 		(response) => {
+// 			if (!response.ok) {
+// 				throw new Error(rersp.statusText);
+// 			}
+// 			return response.json();
+// 		}
+// 	);
+// }
+
+// getTrending()
+// 	.then((data) => {
+// 		list.insertAdjacentHTML("beforeend", createMarkup(data.results));
+// 		//викликаємо метод, спостерігач спостерігай
+// 		observer.observe(target);
+// 		// loadMore працює після того як відмальована вже перша сторінка
+// 		if (data.page !== data.total_pages) {
+// 			loadMore.hidden = false;
+// 		}
+// 	})
+
+// 	.catch((err) => console.log(err));
+
+// function createMarkup(arr) {
+// 	return arr
+// 		.map(
+// 			({ poster_path, title }) => `   <li>
+//         <img src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}">
+//         <h2>${title}</h2>
+//       </li>`
+// 		)
+// 		.join("");
+// }
+
 const target = document.querySelector(".js-guard");
+
 const BASE_URL = "https://api.themoviedb.org/3/";
-const ENDPOINT = "trending/movie/day";
+const END_POINT = "trending/movie/day";
 const API_KEY = "345007f9ab440e5b86cef51be6397df1";
 const list = document.querySelector(".js-list");
 
@@ -20,6 +119,8 @@ function onLoad(entries, observer) {
 			getTrending(currentPage)
 				.then((data) => {
 					list.insertAdjacentHTML("beforeend", createMarkup(data.results));
+					// лише після того,як ми намалювали розмітку ми додаємо прослуховувача подій
+					//викликаємо метод, спостерігач спостерігай
 					if (data.page === data.total_pages) {
 						observer.unobserve(target);
 					}
@@ -32,25 +133,25 @@ function onLoad(entries, observer) {
 function createMarkup(arr) {
 	return arr
 		.map(
-			({ poster_path, title }) => `<li>
-    <img src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}">
-    <h2>${title}</h2>
-</li>`
+			({ poster_path, title }) => `   <li>
+        <img src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}">
+        <h2>${title}</h2>
+      </li>`
 		)
 		.join("");
 }
 
 function getTrending(page = 1) {
-	return fetch(`${BASE_URL}${ENDPOINT}?api_key=${API_KEY}&page=${page}`).then(
-		(resp) => {
-			if (!resp.ok) {
-				throw new Error(resp.statusText);
+	return fetch(`${BASE_URL}${END_POINT}?api_key=${API_KEY}&page=${page}`).then(
+		(response) => {
+			if (!response.ok) {
+				throw new Error(response.statusText);
 			}
-
-			return resp.json();
+			return response.json();
 		}
 	);
 }
+
 getTrending()
 	.then((data) => {
 		list.insertAdjacentHTML("beforeend", createMarkup(data.results));
